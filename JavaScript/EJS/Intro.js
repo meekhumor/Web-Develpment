15dad73671d250375205e753e6efd431d6c715aa
Om Mukherjee <om17092004@gmail.com>
2024-06-29 11:34:26 +0530

    Web Development

--- JavaScript/EJS/Intro.js
+++
@@ -1,2 +0,0 @@
-// EJS (Embedded JavaScript) is a template engine for JavaScript commonly used in web development. 
-// It allows you to generate dynamic HTML markup using JavaScript code within HTML templates. 
--- JavaScript/EJS/Templating/Templating.js
+++
@@ -1,1 +0,0 @@
-// Templating allows us to define a preset pattern for webpage that we can dynamically modify
