0391c104794a4debc21feb408b32ddcd9b92fdb3
Om Mukherjee <om17092004@gmail.com>
2024-06-29 15:53:01 +0530

    Web Development

--- MERN/03_Mongo_DB/03_Cookies/routes/index.js
+++ MERN/03_Mongo_DB/03_Cookies/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Create cookie
 router.get('/', function(req, res, next) {
+  res.cookie("age", 25) // cookie (age = 25) is set in webpage where the client can read it
   res.render('index', { title: 'Express' });
+});
+
+// Read cookie
+router.get('/read', function(req, res, next) {
+  console.log(req.cookies); // { age: '25' }
+  res.send('Cookie read, see terminal');
+});
+
+// Delete cookie
+router.get('/delete', function(req, res, next) {
+  res.clearCookie('age'); // cookie (age = 25) is deleted in webpage where the client can read it
+  res.send('Cookie deleted, see terminal');
 });
 
 module.exports = router;
