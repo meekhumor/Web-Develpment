1d8fddb9736cbe7b2e2599aebaaa85f83a4dce55
Om Mukherjee <om17092004@gmail.com>
2024-11-05 17:45:01 +0530

    Web Development

---
+++ React/07_Context_api/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h2 className='bg-gray-500 text-white'>Context API</h2>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
